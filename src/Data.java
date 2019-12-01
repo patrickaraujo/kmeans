449fa214efe4ae4c1720fa0c4fb86849c6ed6d78
Patrick Araújo <araujo.patrick@live.com>
2019-12-01 16:32:04 -0300

    Update Data.java

--- src/Data.java
+++ src/Data.java
@@ -8,54 +8,24 @@ package kmeans;
  *
  */
 public class Data {
-	private float var1;
-	private float var2;
-	private float var3;
-	private float var4;
+	private float [] var;
 	private String classe;
 	
 	
-	public Data(float var1, float var2, float var3, float var4, String classe) {
-		this.var1 = var1;
-		this.var2 = var2;
-		this.var3 = var3;
-		this.var4 = var4;
+	public Data(float [] var, String classe) {
+		this.var = var;
 		this.classe = classe;
 	}
 
 	
-	public float getVar1() {
-		return var1;
+	public float[] getVar() {
+		return var;
 	}
 
-	public void setVar1(float var1) {
-		this.var1 = var1;
+	public void setVar(float [] var) {
+		this.var = var;
 	}
-
-	public float getVar2() {
-		return var2;
-	}
-
-	public void setVar2(float var2) {
-		this.var2 = var2;
-	}
-
-	public float getVar3() {
-		return var3;
-	}
-
-	public void setVar3(float var3) {
-		this.var3 = var3;
-	}
-
-	public float getVar4() {
-		return var4;
-	}
-
-	public void setVar4(float var4) {
-		this.var4 = var4;
-	}
-
+	
 	public String getClasse() {
 		return classe;
 	}
